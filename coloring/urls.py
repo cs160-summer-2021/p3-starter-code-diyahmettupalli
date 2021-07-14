508ca12be9d08bc8a6eafc4d0c62173a01fa1c71
Diyah Mettupalli <diyahmettupalli@berkeley.edu>
2021-07-13 23:27:46 -0400

    updated routes

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('index', views.index, name='index'),
+    path('demo', views.demo, name='demo'),
+    path('new_interaction', views.new_interaction, name='new_interaction'),
+    path('e', views.e, name='e'),
+    path('f', views.f, name='f'),
+    path('g', views.g, name='g'),
+    path('h', views.h, name='h'),
+    path('i', views.i, name='i'),
+    path('j', views.j, name='j'),
+    path('k', views.k, name='k')
+
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,30 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
+
+def e(request):
+    return render(request, 'coloring/e.html')
+
+def f(request):
+    return render(request, 'coloring/f.html')
+
+def g(request):
+    return render(request, 'coloring/g.html')
+
+def h(request):
+    return render(request, 'coloring/h.html')
+
+def i(request):
+    return render(request, 'coloring/i.html')
+
+def j(request):
+    return render(request, 'coloring/j.html')
+
+def k(request):
+    return render(request, 'coloring/k.html')
